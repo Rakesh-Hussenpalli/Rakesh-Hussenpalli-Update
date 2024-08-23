7e38f985bd72f0b5e56471807efa86000d49e3fb
Rakesh-Hussenpalli <rakeshhussenpalli@gmail.com>
2024-08-23 14:00:22 +0530

    Done

--- src/main/java/UpdateARecord.java
+++ src/main/java/UpdateARecord.java
@@ -9,32 +9,35 @@ import java.sql.Statement;
 /**
  * SQL sublanguage: DML (Data Manipulation Language)
  *
- * Now that we know how to insert and remove records from a table, we are going to learn how to update a record.
+ * Now that we know how to insert and remove records from a table, we are going
+ * to learn how to update a record.
  *
- * To update a record we utilize the UPDATE keyword. The syntax for updating a record is as follows:
- * UPDATE table_name SET col_1 = val_1, col_2 = val_2, ...col_N = val_N WHERE condition;
+ * To update a record we utilize the UPDATE keyword. The syntax for updating a
+ * record is as follows:
+ * UPDATE table_name SET col_1 = val_1, col_2 = val_2, ...col_N = val_N WHERE
+ * condition;
  *
- * NOTE: The WHERE condition is important because if you leave this out, that column will be updated throughout all the records in the table.
+ * NOTE: The WHERE condition is important because if you leave this out, that
+ * column will be updated throughout all the records in the table.
  */
 public class UpdateARecord {
 
     /**
-     *  site_user table:
-     *  |   id  |     firstname        |        lastname        |
-     *  ----------------------------------------------------------
-     *  |1      |'Steve'               |'Garcia'                |
-     *  |2      |'Alexa'               |'Smith'                 |
-     *  |3      |'Steve'               |'Jones'                 |
-     *  |4      |'Brandon'             |'Smith'                 |
-     *  |5      |'Adam'                |'Jones'                 |
+     * site_user table:
+     * | id | firstname | lastname |
+     * ----------------------------------------------------------
+     * |1 |'Steve' |'Garcia' |
+     * |2 |'Alexa' |'Smith' |
+     * |3 |'Steve' |'Jones' |
+     * |4 |'Brandon' |'Smith' |
+     * |5 |'Adam' |'Jones' |
      */
 
-
-    public void problem1(){
+    public void problem1() {
         /**
          * Problem 1: Update Alexa's last name to be 'Rush' in the site_user table.
          */
-        String sql = FileUtil.parseSQLFile("problem1.sql");
+        String sql = "UPDATE site_user SET lastname='Rush' WHERE firstname='Alexa'";
 
         try {
             Connection connection = ConnectionUtil.getConnection();
